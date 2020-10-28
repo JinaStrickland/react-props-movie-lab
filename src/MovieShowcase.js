7829af4033b69f16a757abeda90dcaabcd879125
JinaStrickland <Jina30@gmail.com>
2020-10-28 00:11:30 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,12 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+    return movieData.map((movie, index) => 
+        <MovieCard  key = { index }
+                    title={ movie.title }
+                    rating={ movie.IMDBRating }
+                    genres={ movie.genres }
+                    poster={ movie.poster } />)
   }
 
   render() {
@@ -16,3 +21,11 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+
+// {
+//   title: 'Choux and Maru go to Istanbul',
+//   IMDBRating: 3,
+//   genres: ['cats', 'adventure', 'romance'],
+//   poster: 'choux-maru-istanbul'
+// },
